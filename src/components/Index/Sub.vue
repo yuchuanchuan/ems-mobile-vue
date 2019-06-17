0454cdb20f5875afa36db8726ba8316f46c63e95
chuanchuanyu <1292505843@qq.com>
2019-06-17 22:00:48 +0800

    提交

--- src/components/Index/Sub.vue
+++ src/components/Index/Sub.vue
@@ -367,8 +367,9 @@
           function(res){
             if(res.err_msg == "get_brand_wcpay_request:ok" ) {
               console.log("支付成功")
+              this.$router.push("/Index")
             }     // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
-            location.href = "${returnUrl}";
+            // location.href = "${returnUrl}";
           }
         );
       },
